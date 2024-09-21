167069a51f0af4ab998a4bc85dcb47bb0ee2aa8e
Jean <89807593+naejshaw@users.noreply.github.com>
2024-09-21 14:43:54 -0300

    "Refactor URL parsing in app.ts"

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Routes } from './routes/routes';
 import { HttpMethod } from './utils/httpMethods';
 
 export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const [baseUrl, queryString] = req.url?.split('?') ?? ["", ""]
+    const baseUrl = req.url?.split('?')[0]
     
     if(req.method === HttpMethod.GET && baseUrl === Routes.LIST){
         await getListEpisodes(req, res)
